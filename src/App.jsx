c731d0de6cd6c72e2db46d90876be3207b416552
Tien Nguyen <nguyenvantienthuanan12b3@gmail.com>
2026-05-16 08:12:56 +0700

    added Tips for effectively searching using Tailwind.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,17 @@ import './App.css'
 // import UserProfile from './components/UserProfile';
 // import { userList } from './data/userList';
 function App() {
-  return <h1 className='text-2xl font-bold bg-red-100 rounded-full'>Hello Taiilwin</h1>
+  return (
+  <>
+    <h1 h1 className='text-2xl font-bold bg-red-100 rounded-full'>Hello Taiilwin</h1>  
+    <div className="flex">
+      <button className='bg-red-400 py-2 px-4 text-white rounded-full'>
+        Click me
+      </button>
+    </div>
+  </>
+  )
+
   // Logic
   // console.log(userList);
     // const FilteredUsers = userList.filter(
