34ff67c1b1f9fd386fb009ae3e516eda5858ff4c
Tien Nguyen <nguyenvantienthuanan12b3@gmail.com>
2026-05-17 19:05:27 +0700

    added Custom class-style

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,12 @@ function App() {
   <>
     <h1 h1 className='text-2xl font-bold bg-red-100 rounded-full'>Hello Taiilwin</h1>  
     <div className="flex items-center justify-center px-3 py-2 h-screen">
-      <button className='bg-red-400 py-2 px-4 text-white rounded-full hover:bg-pink-500 md:py-3 md:px-5 cursor-pointer'>
+      {/* <button className='bg-red-400 py-2 px-4 text-white rounded-full hover:bg-pink-500 md:py-3 md:px-5 cursor-pointer'>
         Click me
-      </button>
+      </button> */}
+      <div className='w-[230px] bg-[#0d4242] text-[#fff] p-[30px] hover:bg-pink-600'>
+        React go
+      </div>
     </div>
   </>
   )
